1a89034603ba3f6fdd2953fccf8a5b525fa52e9b
MoscatN <moscatleon0426@gmail.com>
2023-03-07 16:14:31 -0400

    Transactions Completed

--- src/Components/Transaction/Transaction.jsx
+++ src/Components/Transaction/Transaction.jsx
@@ -1,46 +1,36 @@
-import useAccountingEntry from "../../hooks/useAccountingEntry"
+import useTransaction from "../../hooks/useTransaction"
 
-const AccountingEntry = ({accountingEntry}) => {
+const Transaction = ({transaction}) => {
 
-    const { setEdit, deleteAccountingEntry } = useAccountingEntry(); 
-    const { accountingEntryId, description, customerId, account, movementType, accountingEntryDate, accountingEntryAmount, state } = accountingEntry;
+    const { setEdit, deleteTransaction } = useTransaction(); 
+    const { transactionId, movementType, documentId, transactionDate, customerId, amount} = transaction;
   return (
       
           <tr>
               <td className="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap">
-                  {accountingEntryId}
+                  {transactionId}
               </td>
              
               <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">
-                  {description}
+                  {movementType}
               </td>
              
               <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">
-                  {customerId}
+                  {documentId}
               </td>
              
               <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">
-                   {account}
+                   {transactionDate}
                   
               </td>
               
               <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">
-                   {movementType}
+                   {customerId}
                   
               </td>
               
               <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">
-                   {accountingEntryDate}
-                  
-              </td>
-
-              <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">
-                   {accountingEntryAmount}
-                  
-              </td>
-
-              <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">
-                   {state}
+                   {amount}
                   
               </td>
 
@@ -48,17 +38,17 @@ const AccountingEntry = ({accountingEntry}) => {
                   <button 
                       type="button"
                       className="text-green-500 hover:Text-green-700 rounded-lg"
-                    onClick={()=>setEdit(accountingEntry)}>Editar</button>
+                    onClick={()=>setEdit(transaction)}>Editar</button>
               </td>
               <td className="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
               <button 
                       type="button"
                       className="text-red-500 hover:Text-red-700 rounded-lg"
-                    onClick={()=>deleteAccountingEntry(accountingEntryId)}>Borrar</button>
+                    onClick={()=>deleteTransaction(transaction)}>Borrar</button>
                   
               </td>
           </tr>
   )
 }
 
-export default AccountingEntry;
+export default Transaction;

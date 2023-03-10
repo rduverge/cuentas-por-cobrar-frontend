029677fc7ba248113ec245b2bf799974971f2091
Moscat <moscatleon0426@gmail.com>
2023-03-10 17:23:07 -0400

    Customer & Transaction Validators

--- src/Validations/TransactionValidator.js
+++ src/Validations/TransactionValidator.js
@@ -2,9 +2,18 @@ import * as yup from 'yup';
 
 const TransactionSchema = yup.object().shape({
 
-    movementType: yup.string().required("Debe seleccionar un Tipo de Movimiento."),
-    document: yup.number().required("Debe seleccionar un documento."),
-    transactionDate: yup.date().max(Date.now(), "La fecha no puede ser en el futuro."),
-    customer: yup.number().required("Debe seleccionar un cliente."),
-    amount: yup.number().required().positive()
+    movementType: yup.string()
+    .required("Debe seleccionar un Tipo de Movimiento."),
+    
+    document: yup.number()
+    .required("Debe seleccionar un documento."),
+    
+    transactionDate: yup.date()
+    .max(new Date(), "La fecha no puede ser en el futuro."),
+    
+    customer: yup.number()
+    .required("Debe seleccionar un cliente."),
+    
+    amount: yup.number()
+    .required("Debe ingresar un monto").positive("El monto no puede ser menor a 0.")
 })
